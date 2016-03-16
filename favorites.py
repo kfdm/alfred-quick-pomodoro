4e8b9873fa77852cdbac5e99bf64f479ef0edc15
Paul Traylor <kungfudiscomonkey@gmail.com>
2016-03-16 13:21:30 +0900

    Add support for icons and move API_KEY to settings

--- favorites.py
+++ favorites.py
@@ -1,21 +1,32 @@
 # encoding: utf-8
 
+import os
 import sys
 import workflow
+import workflow.web as web
 
+FAVORITES_URL = 'http://tsundere.co/api/favorite/?format=json'
 
 def main(wf):
-    params = dict(auth_token=API_KEY, count=20, format='json')
-    headers = {'Authorization': 'Token %s' % API_KEY}
+    params = dict(count=20, format='json')
+    headers = {'Authorization': 'Token %s' % wf.settings['API_KEY']}
     r = workflow.web.get(FAVORITES_URL, params, headers)
     r.raise_for_status()
 
     for favorite in r.json()['results']:
+        icon = wf.datafile(favorite['title'])
+        if os.path.exists(icon) is False:
+            if favorite['icon']:
+                request = web.get(favorite['icon'])
+                request.save_to_path(icon)
+            else:
+                icon = workflow.ICON_CLOCK
         wf.add_item(
             title=favorite['title'],
             subtitle=favorite['category'],
             valid=True,
-            arg=u'tell application "Pomodoro" to start "{title} #{category}" duration {duration}'.format(**favorite)
+            arg=u'tell application "Pomodoro" to start "{title} #{category}" duration {duration}'.format(**favorite),
+            icon=icon,
         )
     wf.send_feedback()
 
