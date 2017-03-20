b857821ecdc2f9cc387c0d1d465d2af6d5baefa2
Paul Traylor <kungfudiscomonkey@gmail.com>
2017-03-20 16:41:42 +0900

    utf8 fixes

--- parse.py
+++ parse.py
@@ -25,9 +25,9 @@ def main(wf):
     r = requests.post(FAVORITES_URL, data=json.dumps(pomodoro), headers=headers)
 
     if r.ok:
-        print(u'{title} {end}'.format(**r.json()))
+        print(u'{title} {end}'.format(**r.json()).encode('utf8', 'ignore'))
     elif r.status_code == 409:
-        print(u'Active Pomodoro: {title} {end}'.format(status=r.status_code, **r.json()))
+        print(u'Active Pomodoro: {title} {end}'.format(status=r.status_code, **r.json()).encode('utf8', 'ignore'))
     else:
         print(r.text)
 
