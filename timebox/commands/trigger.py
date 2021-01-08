dfcf532d63c9683bce7b566d1ca52d05f06ce441
Paul Traylor <kungfudiscomonkey@gmail.com>
2021-01-08 20:09:56 +0900

    Update trigger script

--- timebox/commands/trigger.py
+++ timebox/commands/trigger.py
@@ -1,23 +1,23 @@
-#!/usr/bin/env python
-# encoding: utf-8
-
+import logging
 import sys
+
 import requests
-import workflow
+from timebox import auth, settings
 
-FAVORITES_URL = 'https://tsundere.co/api/favorite/{id}/start'
+logger = logging.getLogger(__name__)
 
 
-def main(wf):
-    headers = {'Authorization': 'Token %s' % wf.settings['API_KEY']}
-    r = requests.post(FAVORITES_URL.format(id=wf.args[0]), headers=headers)
+def main():
+    url = "{}/favorite/{}/start".format(settings.API_BASE, sys.argv[1])
+    r = requests.post(url, auth=auth.TokenAuth(settings.API_KEY))
 
     if r.ok:
-        print(u'{title} {end}'.format(**r.json()).encode('utf8', 'ignore'))
+        print("{title} {end}".format(**r.json()))
     elif r.status_code == 409:
-        print(u'Cannot replace active Pomodoro: {title} {end}'.format(status=r.status_code, **r.json()['data']).encode('utf8', 'ignore'))
+        print(
+            "Cannot replace active Pomodoro: {title} {end}".format(
+                status=r.status_code, **r.json()["data"]
+            )
+        )
     else:
         print(r.text)
-
-if __name__ == '__main__':
-    sys.exit(workflow.Workflow().run(main))
