4ca2da67ce11db6239419bc56d9160bf85a08433
Paul Traylor <kungfudiscomonkey@gmail.com>
2021-01-08 20:56:17 +0900

    Setting reordering

--- timebox/settings.py
+++ timebox/settings.py
@@ -3,23 +3,31 @@ import pathlib
 import json
 import logging
 
-logging.basicConfig(level=logging.DEBUG)
+# Alfred Defaults
+LIBRARY = pathlib.Path().home() / "Library"
+ALFRED_CACHE = LIBRARY / "Caches" / "com.runningwithcrayons.Alfred"
+ALFRED_DATA = LIBRARY / "Application Support" / "Alfred" / "Workflow Data"
+
+# System Default
+ICON_ROOT = pathlib.Path(
+    "/System/Library/CoreServices/CoreTypes.bundle/Contents/Resources"
+)
+
+# Alfred Environment Settings
+
+if os.environ.get("alfred_debug") == "1":
+    logging.basicConfig(level=logging.DEBUG)
+else:
+    logging.basicConfig(level=logging.INFO)
 
 BUNDLE_ID = os.environ.get(
     "alfred_workflow_bundleid", "net.kungfudiscomonkey.alfred.quickpomodoro"
 )
-ALFRED_CACHE = (
-    pathlib.Path().home() / "Library" / "Caches" / "com.runningwithcrayons.Alfred"
-)
-ALFRED_VERSION = os.environ.get("alfred_version", "Unknown")
-ALFRED_DATA = (
-    pathlib.Path().home()
-    / "Library"
-    / "Application Support"
-    / "Alfred"
-    / "Workflow Data"
-)
 
+ALFRED_VERSION = os.environ.get("alfred_version", "Unknown")
+
+
+# Remaining derived settings
 
 if "alfred_workflow_data" in os.environ:
     WORKFLOW_DIR = pathlib.Path(os.environ["alfred_workflow_data"])
@@ -38,6 +46,3 @@ with (WORKFLOW_DIR / "settings.json").open() as fp:
 API_KEY = SETTINGS.get("API_KEY")
 API_BASE = SETTINGS.setdefault("API_ENDPOINT", "https://tsundere.co/api")
 USER_AGENT = "alfred/{}/{}".format(ALFRED_VERSION, BUNDLE_ID)
-ICON_ROOT = pathlib.Path(
-    "/System/Library/CoreServices/CoreTypes.bundle/Contents/Resources"
-)
